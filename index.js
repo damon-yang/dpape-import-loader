e30435dff7ac959762537a168adb06c5633b66bc
dyang <yang2015damon@outlook.com>
2019-07-25 17:32:50 +0800

    修复issue

--- index.js
+++ index.js
@@ -2,30 +2,29 @@ const { getOptions } = require('loader-utils');
 
 const buildLibrar = (source,moduleName,global,libraryDirectory) => {
   if(global){
-    var allReg = new RegExp(`import[\\s+][\\w]*[\\s+]from[\\s+][\\'|\\"]${moduleName}[\\'|\\"][\\s+]?;`,"g");
+    var allReg = new RegExp(`import[\\s+][\\w\\r\\n]*[\\s+]from[\\s+][\\'|\\"]${moduleName}[\\'|\\"][\\s+]?;`,"g");
     source = source.replace(allReg,function(a){
       const gcss = `\nimport '${moduleName}/${libraryDirectory}/index.css';`;
       return a+gcss;
     });
-  }else{
-    const importReg = new RegExp(`import[\\s+][\\{][\\w\\s\\,]*[\\}][\\s+]from[\\s+][\\'|\\"]${moduleName}[\\'|\\"][\\s+]?;`,"g");          
-    source = source.replace(importReg,function(a){      
-      const components = a.split(/[\{|\}]/)[1].split(',');  
-      let singleResult = '';
-      components.map(v=>{
-        const name = (v + '').trim();
-        const arr = name.split('');
-        /** 首字母 */
-        const acronym = (arr[0] || '').toLocaleLowerCase();
-        arr.splice(0,1);
-        const fileName = acronym + arr.join('');
-        const newFileName = fileName.replace(/[A-Z]/g,function(a){return `-${a.toLocaleLowerCase()}`;})
-        singleResult += `\nimport ${name} from '${moduleName}/${libraryDirectory}/${newFileName}/index';`
-        singleResult += `\nimport '${moduleName}/${libraryDirectory}/${newFileName}/style';`
-      });
-      return singleResult;
-    })
   }
+  const importReg = new RegExp(`import[\\s+][\\{][\\w\\s\\,\\r\\n]*[\\}][\\s+]from[\\s+][\\'|\\"]${moduleName}[\\'|\\"][\\s+]?;`,"g");          
+  source = source.replace(importReg,function(a){      
+    const components = a.split(/[\{|\}]/)[1].split(',');  
+    let singleResult = '';
+    components.map(v=>{
+      const name = (v + '').trim();
+      const arr = name.split('');
+      /** 首字母 */
+      const acronym = (arr[0] || '').toLocaleLowerCase();
+      arr.splice(0,1);
+      const fileName = acronym + arr.join('');
+      const newFileName = fileName.replace(/[A-Z]/g,function(a){return `-${a.toLocaleLowerCase()}`;})
+      singleResult += `\nimport ${name} from '${moduleName}/${libraryDirectory}/${newFileName}/index';`
+      singleResult += `\nimport '${moduleName}/${libraryDirectory}/${newFileName}/style';`
+    });
+    return singleResult;
+  })
   return source;
 }
 
