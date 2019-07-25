59c10a3bd5ed17b5cf19867ea22f52c1be30b72f
dyang <yang2015damon@outlook.com>
2019-07-25 15:55:53 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # dyang-import-loader
-typescript | js 按需引入工具
+组件库按需引入工具支持（TS | JS | JSX | TSX）
+## 使用方式 Install
+```shell
+npm i dyang-import-loader -D
+```
+### Loader options
+```javascript
+    ...
+    {
+        test: /\.(jsx|tsx|js|ts)$/,
+        exclude: /node_modules/,
+        loader: "dyang-import-loader",
+        options:{
+            /* 是否全局引入默认为true,需要按需引入设置为false*/
+            global:true,
+            /** 按需引入目录名称 默认lib*/
+            libraryDirectory:'lib',
+            /* 组件库名称 默认vue-dyangui*/
+            libraryName:'vue-dyangui'
+        }
+    }
+    ...
+```
+
+### 组件库目录结构
+```
+├──lib
+│   ├──Example component1       //按需引入组件文件名
+│   │   ├──index.js             //组件代码
+│   │   ├──style                //组件样式文件夹
+│   │   │   |──index.css        //组件样式
+│   │   │   |──index.js         //组件样式引入js文件
+│   ├──index.js                 //全局引入js文件
+│   ├──index.css                 //全局引入css文件
+├──package.json
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+const { getOptions } = require('loader-utils');
+
+module.exports = function(source){
+    /**
+     * 获取loader参数
+     */
+    const options = getOptions(this) || new Object();
+    const isGlobal = options.global === undefined ? true : options.global;
+    const moduleName = options.libraryName === undefined ? 'vue-dyangui' : options.libraryName;
+    const libraryDirectory = options.libraryDirectory === undefined ? 'lib' : options.libraryDirectory;
+    if(isGlobal){
+      var allReg = new RegExp(`import[\\s+][\\w]*[\\s+]from[\\s+][\\'|\\"]${moduleName}[\\'|\\"][\\s+]?;`,"g");
+      source = source.replace(allReg,function(a){
+        const gcss = `\nimport '${moduleName}/${libraryDirectory}/index.css';`;
+        return a+gcss;
+      });
+    }else{
+      const importReg = new RegExp(`import[\\s+][\\{][\\w\\s\\,]*[\\}][\\s+]from[\\s+][\\'|\\"]${moduleName}[\\'|\\"][\\s+]?;`,"g");          
+      source = source.replace(importReg,function(a){      
+        const components = a.split(/[\{|\}]/)[1].split(',');  
+        let singleResult = '';
+        components.map(v=>{
+          const name = (v + '').trim();
+          const arr = name.split('');
+          /** 首字母 */
+          const acronym = (arr[0] || '').toLocaleLowerCase();
+          arr.splice(0,1);
+          const fileName = acronym + arr.join('');
+          const newFileName = fileName.replace(/[A-Z]/g,function(a){return `-${a.toLocaleLowerCase()}`;})
+          singleResult += `\nimport ${name} from '${moduleName}/${libraryDirectory}/${newFileName}/index';`
+          singleResult += `\nimport '${moduleName}/${libraryDirectory}/${newFileName}/style';`
+        });
+        return singleResult;
+      })
+    }
+    return source;
+}
